765b04ae34b31f59b7111656afc59a2c831a4b94
Roshan Maharjan <roshanmaharjan678@gmail.com>
2024-08-24 12:57:23 +0545

    Category Card Responsive

--- src/components/pages/HomePage/Categories.tsx
+++ src/components/pages/HomePage/Categories.tsx
@@ -4,7 +4,7 @@ const Categories = () => {
   return (
     <div className="py-[80px] space-y-10">
         <h2 className="text-[1.8rem]">Recipe <span className="text-[#fb780e]">Category</span></h2>
-        <div className="flex gap-4">
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-4">
           <CategoryCard/>
           <CategoryCard/>
           <CategoryCard/>
